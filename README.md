7a73f0cd4e57c2fad1bc9f51bf3af6e145debd4b
KIM Kiyeong <walkst26@gmail.com>
2022-04-12 23:37:15 +0800

    add first card practice code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # udemy-go-guide
+
+This repository is for practice of Udemy course (https://rakuten.udemy.com/course/go-the-complete-developers-guide/learn/quiz/391900#reviews)
---
+++ cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// automatically called
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+	// Only a "string" will be ever assinged to this variable
+	fmt.Println(card)
+}
